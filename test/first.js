af9dd82e167738a5e7fa29a38558e2c10579cf4e
jinux <jinguol999@163.com>
2016-09-13 00:07:01 +0800

    change: 让测试通过

--- test/first.js
+++ test/first.js
@@ -1,7 +1,9 @@
 var Application = require('spectron').Application
 var assert = require('assert')
 
-var app_path = '../dist/MyApp-linux-x64/MyApp'
+var app_path = 'MyApp-linux-x64/MyApp'
+
+console.log(process.cwd())
 
 describe('application launch', function () {
   this.timeout(10000)
@@ -21,7 +23,7 @@ describe('application launch', function () {
 
   it('shows an initial window', function () {
     return this.app.client.getWindowCount().then(function (count) {
-      assert.equal(count, 1)
+      assert.equal(count, 2) // 菜单也算一个?
     })
   })
 })
