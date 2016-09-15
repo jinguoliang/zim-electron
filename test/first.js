87993f7a95cf2b30895446bc5a32ade90a718266
jinux <jinguol999@163.com>
2016-09-15 12:14:43 +0800

    [build]调整测试目录

--- test/first.js
+++
@@ -1,7 +0,0 @@
-var assert = require('assert')
-
-describe('first test', function () {
-  it('test add', function () {
-      assert.equal(1 + 1, 2) 
-  })
-})
---
+++ test/unittest/markdown.test.js
@@ -0,0 +1,15 @@
+var assert = require('assert')
+// var assert = require
+var markdown = require('markdown').markdown
+
+var fs = require('fs');
+var front = require('hexo-front-matter');
+
+describe('hexo-front-matter', function () {
+  it('parse header of md file', function () {
+ 	var data = fs.readFileSync('test/data/test_has_front.md', 'utf-8');
+	d = front.parse(data);
+	// assert.equal(d.title, '[hello <hello')
+	assert.equal(d.title, 'hello hello')
+  })
+})
